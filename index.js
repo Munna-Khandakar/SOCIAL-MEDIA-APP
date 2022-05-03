50880a048c29f9d2f51516e0dab78eefc62c2855
Munna-Khandakar <munnashisad@gmail.com>
2022-05-03 22:47:05 +0600

    project initiated

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
+// main app initialization
+const app = express();
+dotenv.config();
+
+// mongodb connection
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("DB connected...");
+  }
+);
+
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// routes
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+// app listenning ...
+app.listen(5000, () => {
+  console.log("backend server is running on port 5000");
+});
